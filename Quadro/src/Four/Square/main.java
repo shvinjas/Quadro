5b417b4c80ece85d4df05aa52614a9b23124049c
Gloria <Gloria@Gloria-PC>
2022-12-29 03:21:39 +1300

    third commit

---
+++ Quadro/src/Four/Square/main.java
@@ -0,0 +1,13 @@
+package Four.Square;
+import java.util.Scanner;
+public class main
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Whats your name?");
+		String name = sc.next();
+		System.out.println("welcome"+"  " +name+","+" "+"Have fun !");
+	}
+
+}
